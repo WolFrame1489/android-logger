f4d354868158b55ae8c8d16c428b5898ccc75841
WolFrame1488 <dlaritchev@hotmail.com>
2021-02-21 14:43:26 +0300

    гироскоп
    показывает 3 г8рафика углов

--- Assets/Scripts/graphscript.cs
+++ Assets/Scripts/graphscript.cs
@@ -5,24 +5,41 @@ using static System.Math;
 
 public class graphscript : MonoBehaviour
 {
-    public Transform tochka;
+    public Transform xtochka;
+    public Transform ytochka;
+    public Transform ztochka;
     public GameObject pixelholder;
     public Vector3 resvec;
-    public float i = 0;
+    public float i = 50f;
     public float res = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Input.gyro.enabled = true;
     }
 
     // Update is called once per frame
     void Update()
     {
         i+= 0.5f;
-        res = Mathf.Cos(i);
-        resvec = new Vector3(i, res + 200, 1);
-        Instantiate(tochka, resvec, Quaternion.identity);
+        res = Input.gyro.attitude.eulerAngles.x;
+        resvec = new Vector3(i, (res + 200), 1);
+        Instantiate(xtochka, resvec, Quaternion.identity);
+        res = Input.gyro.attitude.eulerAngles.y;
+        resvec = new Vector3(i, (res + 200), 1);
+        Instantiate(ytochka, resvec, Quaternion.identity);
+        res = Input.gyro.attitude.eulerAngles.z;
+        resvec = new Vector3(i, (res + 200), 1);
+        Instantiate(ztochka, resvec, Quaternion.identity);
 
     }
-}
+    protected void OnGUI()
+    {
+        GUI.skin.label.fontSize = Screen.width / 40;
+
+        GUILayout.Label("Orientation: " + Screen.orientation);
+        GUILayout.Label("input.gyro.attitude: " + Input.gyro.attitude.eulerAngles.x);
+        GUILayout.Label("input.gyro.attitude: " + Input.gyro.attitude.eulerAngles.y);
+        GUILayout.Label("input.gyro.attitude: " + Input.gyro.attitude.eulerAngles.z);
+    }
+    }
